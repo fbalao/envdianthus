68773e8ebde9e54761b4217082ccf6749ad3bbed
JavLopezJurado <javlopez@us.es>
2017-10-13 18:10:07 +0200

    script zoon package

--- zoon_broteri.R
+++ zoon_broteri.R
@@ -1,1 +1,38 @@
+library(gtools)
+library(rgdal)
 library(zoon)
+library(spThin)
+
+
+dbroteri <- read.csv2 (file="D:/Copia de seguridad JAVI/UNIVERSIDAD DE SEVILLA/Experimentos Dianthus/Lopez_Juradoetal2017_nicho/dbroteri.csv")
+
+e <- extent (-10,3,35,44)
+chelsafiles <- mixedsort (list.files ("D:/Copia de seguridad JAVI/UNIVERSIDAD DE SEVILLA/Experimentos Dianthus/Lopez_Juradoetal2017_nicho/chelsa", pattern = ".tif", full.names = TRUE))
+chelsa <- stack (chelsafiles)
+che.c <- crop (chelsa,e)
+
+enviremfiles <- mixedsort (list.files ("D:/Copia de seguridad JAVI/UNIVERSIDAD DE SEVILLA/Experimentos Dianthus/Lopez_Juradoetal2017_nicho/envirem", pattern = ".bil", full.names = TRUE))
+envirem <- stack (enviremfiles)
+env.c <- crop (envirem, e)
+
+alt15files <- mixedsort (list.files ("D:/Copia de seguridad JAVI/UNIVERSIDAD DE SEVILLA/Experimentos Dianthus/Lopez_Juradoetal2017_nicho/altitud_15", pattern = ".bil", full.names = TRUE))
+alt16files <- mixedsort (list.files ("D:/Copia de seguridad JAVI/UNIVERSIDAD DE SEVILLA/Experimentos Dianthus/Lopez_Juradoetal2017_nicho/altitud_16", pattern = ".bil", full.names = TRUE))
+alt15 <- stack (alt15files)
+alt16 <- stack (alt16files)
+alt.m <- merge (alt15, alt16, ext=e)
+
+soilgridsfiles <- mixedsort (list.files ("D:/Copia de seguridad JAVI/UNIVERSIDAD DE SEVILLA/Experimentos Dianthus/Lopez_Juradoetal2017_nicho/soilgrids/capas", pattern = ".tif", full.names = TRUE))
+soilgrids <- stack (soilgridsfiles)
+soil.c <- crop (soilgrids, e) 
+
+
+crs (env.c) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
+crs (alt.m) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
+variables0 <- CombineRasters (list(che.c, env.c, alt.m, soil.c))
+varsstack <- stack(variables0[[1]], variables0[[2]], variables0[[3]], variables0[[4]])
+
+work <- workflow (occurrence = LocalOccurrenceData (filename="D:/Copia de seguridad JAVI/UNIVERSIDAD DE SEVILLA/Experimentos Dianthus/Lopez_Juradoetal2017_nicho/dbroteri.csv"),
+                  covariate  = LocalRaster(varsstack),
+                  process    = Background (n = 1000, bias = 50),
+                  model      = MaxEnt,
+                  output     = PrintMap)
