eb5c127d6c39bfebf894582f0b0784411cbdbf31
JavLopezJurado <javlopez@us.es>
2017-11-21 23:39:11 +0100

    new

--- zoon_server_javi.R
+++ zoon_server_javi.R
@@ -1,45 +1,50 @@
 library (zoon)
 library (future)
 library (ulimit)
-# library (spatialEco)
+library (gtools)
+library (spatialEco)
 
 #==================POPULATIONS=====================#
 
 # dbrot <- LocalOccurrenceData (filename="dbroteri.csv")
 # 
-# e <- extent (-10,4.5,35.5,44)
-# chefiles <- mixedsort (list.files ("D:/Copia de seguridad JAVI/UNIVERSIDAD DE SEVILLA/Experimentos Dianthus/Lopez_Juradoetal2018_nicho/vars_selected_pops/chelsa", pattern = ".tif", full.names = TRUE))
-# chelsa <- stack (chefiles)
-# che.c <- crop (chelsa,e)
-# 
-# envfiles <- mixedsort (list.files ("D:/Copia de seguridad JAVI/UNIVERSIDAD DE SEVILLA/Experimentos Dianthus/Lopez_Juradoetal2018_nicho/vars_selected_pops/envirem", pattern = ".bil", full.names = TRUE))
-# envirem <- stack (envfiles)
-# env.c <- crop (envirem,e)
-# 
-# soilfiles <- mixedsort (list.files ("D:/Copia de seguridad JAVI/UNIVERSIDAD DE SEVILLA/Experimentos Dianthus/Lopez_Juradoetal2018_nicho/vars_selected_pops/soil", pattern = ".tif", full.names = TRUE))
-# soilgrids <- stack (soilfiles)
-# soil.c <- crop (soilgrids,e)
-# 
-# alt15files <- mixedsort (list.files ("D:/Copia de seguridad JAVI/UNIVERSIDAD DE SEVILLA/Experimentos Dianthus/Lopez_Juradoetal2018_nicho/altitud_15", pattern = ".bil", full.names = TRUE))
-# alt16files <- mixedsort (list.files ("D:/Copia de seguridad JAVI/UNIVERSIDAD DE SEVILLA/Experimentos Dianthus/Lopez_Juradoetal2018_nicho/altitud_16", pattern = ".bil", full.names = TRUE))
-# alt15 <- stack (alt15files)
-# alt16 <- stack (alt16files)
-# alt.m <- merge (alt15, alt16, ext=e)
-# tri.ext <- tri(alt.m)
-# 
-# projection (che.c) <- "+proj=longlat +ellps=WGS84 +datum=WGS84"
-# projection (env.c) <- "+proj=longlat +ellps=WGS84 +datum=WGS84"
-# projection (soil.c) <- "+proj=longlat +ellps=WGS84 +datum=WGS84"
-# projection (tri.ext) <- "+proj=longlat +ellps=WGS84 +datum=WGS84"
-# combras <- CombineRasters(c(che.c, env.c, soil.c, tri.ext))
-# vars.stack <- stack (combras [[1]], combras [[2]], combras [[3]], combras[[4]])
-# writeRaster(vars.stack,"stack_zoon_pops.grd", format="raster")
+e <- extent (-10,4.5,35.5,44)
+chefiles <- mixedsort (list.files ("D:/Copia de seguridad JAVI/UNIVERSIDAD DE SEVILLA/Experimentos Dianthus/Lopez_Juradoetal2018_nicho/vars_selected_gbif/chelsa", pattern = ".tif", full.names = TRUE))
+chelsa <- stack (chefiles)
+che.c <- crop (chelsa,e)
+
+alt15files <- mixedsort (list.files ("D:/Copia de seguridad JAVI/UNIVERSIDAD DE SEVILLA/Experimentos Dianthus/Lopez_Juradoetal2018_nicho/altitud_15", pattern = ".bil", full.names = TRUE))
+alt16files <- mixedsort (list.files ("D:/Copia de seguridad JAVI/UNIVERSIDAD DE SEVILLA/Experimentos Dianthus/Lopez_Juradoetal2018_nicho/altitud_16", pattern = ".bil", full.names = TRUE))
+alt15 <- stack (alt15files)
+alt16 <- stack (alt16files)
+alt.m <- merge (alt15, alt16, ext=e)
+
+envfiles <- mixedsort (list.files ("D:/Copia de seguridad JAVI/UNIVERSIDAD DE SEVILLA/Experimentos Dianthus/Lopez_Juradoetal2018_nicho/vars_selected_gbif/envirem", pattern = ".bil", full.names = TRUE))
+envirem <- stack (envfiles)
+env.c <- crop (envirem,e)
+
+soilfiles <- mixedsort (list.files ("D:/Copia de seguridad JAVI/UNIVERSIDAD DE SEVILLA/Experimentos Dianthus/Lopez_Juradoetal2018_nicho/vars_selected_gbif/soil", pattern = ".tif", full.names = TRUE))
+soilgrids <- stack (soilfiles)
+soil.c <- crop (soilgrids,e)
+
+tri.ext <- tri(alt.m)
+
+projection (che.c) <- "+proj=longlat +ellps=WGS84 +datum=WGS84"
+projection (alt.m) <- "+proj=longlat +ellps=WGS84 +datum=WGS84"
+projection (env.c) <- "+proj=longlat +ellps=WGS84 +datum=WGS84"
+projection (soil.c) <- "+proj=longlat +ellps=WGS84 +datum=WGS84"
+projection (tri.ext) <- "+proj=longlat +ellps=WGS84 +datum=WGS84"
+
+combras <- CombineRasters(c(che.c, env.c, soil.c, tri.ext))
+vars.stack <- stack (combras [[1]], combras [[2]], combras [[3]], combras[[4]])
+writeRaster(vars.stack,"stack_zoon_gbif.grd", format="raster")
+
 
 # Chain(PrintMap, PerformanceMeasures),
 
 LoadModule('PerformanceMeasures')
 
-vars.stack <- stack("stack_zoon_pops.grd")
+vars.stack <- stack("D:/Copia de seguridad JAVI/UNIVERSIDAD DE SEVILLA/Experimentos Dianthus/Lopez_Juradoetal2018_nicho/stack_zoon_gbif.grd")
 plan (multicore)
 
 work <- function(){ workflow (occurrence = LocalOccurrenceData (filename="dbroteri.csv",
@@ -63,13 +68,11 @@ save(workres, file = 'workflow_javi.RData')
 LoadModule('PerformanceMeasures')
 LoadModule('ROCcurve')
 
-vars.stack.gbif <- stack("stack_zoon_gbif.grd")
+vars.stack.gbif <- stack("stack_zoon_alllayers.grd")
 plan (multicore)
 ulimit::memory_limit(100000)
 
-workgbif <- function(){ workflow (occurrence = Chain (LocalOccurrenceData 
-                               (filename = "dbroterigbif.csv"), LocalOccurrenceData 
-                               (filename = "externalvalidation.csv", externalValidation = TRUE)),
+workgbif <- function(){ workflow (occurrence = LocalOccurrenceData (filename = "dbroterigbif.csv"),
                               covariate  = LocalRaster(vars.stack.gbif),
                               process = Chain (Background (n = 10000, bias = 100, seed = 999999), 
                                                StandardiseCov),
